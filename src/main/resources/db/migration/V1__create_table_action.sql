0958f5321386c72aebfff8bac0bb6aa90c3aee37
Brendo Vale <brendo.vale02@gmail.com>
2022-07-23 14:00:51 -0400

    fix database transaction migration

--- src/main/resources/db/migration/V1__create_table_action.sql
+++ src/main/resources/db/migration/V1__create_table_action.sql
@@ -1,5 +1,5 @@
 create table transaction(
-    id BIGINT NOT NULL AUTO_INCREMENT,
+    id bigint not null auto_increment,
     title VARCHAR(50) NOT NULL,
     amount DECIMAL(15,2) NOT NULL,
     release_type VARCHAR(50) NOT NULL,
@@ -7,4 +7,6 @@ create table transaction(
     PRIMARY KEY (id)
 );
 
-INSERT INTO transaction VALUES(1, 'Curso Alura', 980.02, 'DEBIT', NOW());
+INSERT INTO transaction(
+    title,amount,release_type,created_at
+) VALUES('Curso Alura', 980.02, 'DEBIT', NOW());
